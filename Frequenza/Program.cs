6f82c0384ccfdf81f0cfc4127b6d49509fb8de30
0d1n92 <rombaldonienrico@gmail.com>
2024-06-04 11:39:57 +0200

    init

---
+++ Frequenza/Program.cs
@@ -0,0 +1,48 @@
+﻿
+class Program
+{
+    static int GetMaxFrequente(int[] array)
+    {
+       
+        var query = Query(array);
+        
+        return query.First().Numero;
+    }
+
+    private static (int Numero, int Frequenza)[] Query(int[] array)
+    {
+        var frequenzaQuery = array
+            .GroupBy(numero => numero)
+            .Select(gruppo => (Numero: gruppo.Key, Frequenza: gruppo.Count()))
+            .OrderByDescending(obj => obj.Frequenza)
+            .ThenBy(obj => obj.Numero)
+            .ToArray();
+
+        return frequenzaQuery;
+    }
+    static int[] GetMaxFrequenti(int[] array, int k)
+    {
+        return Query(array).Select(risultato => risultato.Numero).Take(k).ToArray();
+    }
+
+
+    static void Main()
+    {
+        int[] numeri1 = { 55, 42, 88, 42, 88, 42 };
+        int[] numeri2 = { 1, 52, 52, 1, 56, 1, 54, 54, 1, 54 };
+        int[] numeri3 = { 52, 52, 52, 56, 54, 54, 54 };
+
+        Console.WriteLine($"getMaxFrequente([55, 42, 88, 42, 88, 42]): {GetMaxFrequente(numeri1)}");
+        Console.WriteLine($"getMaxFrequente([1, 52, 52, 1, 56, 1, 54, 54, 1, 54]): {GetMaxFrequente(numeri2)}");
+        Console.WriteLine($"getMaxFrequente([52, 52, 52, 56, 54, 54, 54]): {GetMaxFrequente(numeri3)}");
+
+        int[] numeri4 = { 1, 52, 52, 52, 1, 56, 1, 54, 54, 1, 54 };
+        int[] numeri5 = { 1, 52, 52, 52, 1, 56, 1, 54, 54, 1, 54 };
+        int[] numeri6= { 1, 52, 52, 52, 1, 56, 1, 54, 54, 1, 54 };
+
+        Console.WriteLine($"getMaxFrequenti([1, 52, 52, 52, 1, 56, 1, 54, 54, 1, 54], 1): [{string.Join(", ", GetMaxFrequenti(numeri4,1))}]");
+        Console.WriteLine($"getMaxFrequenti([1, 52, 52, 52, 1, 56, 1, 54, 54, 1, 54], 2): [{string.Join(", ", GetMaxFrequenti(numeri5,2))}]");
+        Console.WriteLine($"getMaxFrequenti([1, 52, 52, 52, 1, 56, 1, 54, 54, 1, 54], 3): [{string.Join(", ", GetMaxFrequenti(numeri6,3))}]");
+
+    }
+}
---
+++ Resistenza/Program.cs
@@ -0,0 +1,47 @@
+﻿
+var res = new Resistenza();
+Console.WriteLine("Inserisci un numero intero per calcololarne la resistenza");
+string input = Console.ReadLine();
+int number;
+Int32.TryParse(input, out number);
+var result = res.GetResistenza(number);
+Console.WriteLine("La resistenza è " + result);
+Console.WriteLine("getResistenza(39): " + res.GetResistenza(39)); 
+Console.WriteLine("getResistenza(77): " + res.GetResistenza(77));
+Console.WriteLine("getResistenza(1679): " + res.GetResistenza(1679)); 
+Console.WriteLine("getResistenza(6788): " + res.GetResistenza(6788)); 
+Console.WriteLine("Resistenza di sette numeri " + res.SearchSevenResult());
+
+
+
+internal class Resistenza
+{
+     public int GetResistenza(int numberInput)
+     {
+         if (numberInput < 10)
+             return 0;
+         int product = 1;
+         while (numberInput > 0)
+         {
+             int cifra = numberInput % 10;
+             product *= cifra;
+             numberInput /= 10;
+         }
+         return 1 + GetResistenza(product);
+     } 
+
+    public int SearchSevenResult ()
+    {
+        var result = 0;
+        var i = 0;
+
+        while (result != 7)
+        {
+            result = GetResistenza(i++);
+
+        }
+
+        return i;
+    }
+
+}
